4f8b84714c5d18992adae8929d838c499d9e9b0d
Srivastava, Pranav <pranavsrivastava108@gmail.com>
2018-09-11 11:30:10 +0200

    added klm-apidaze route

--- routes/klm-apidaze.js
+++ routes/klm-apidaze.js
@@ -3,38 +3,67 @@ var router = express.Router();
 
 /* GET ncco objects */
 router.get('/', function(req, res, next) {
-    var ncco = {
+    var KLMIndex = {
         message: 'KLM Index'
     };
 
-    res.json(ncco);
+    res.json(KLMIndex);
 });
 
-router.get('/externalScript', function(req, res, next) {
-    var actionTalk = {
-        action: "talk",
-        text: "You are joining a conference that was created using CUBE's Voice API."
-    };
+router.get('/externalScript', function(request, response, next) {
+    console.log("request.method    :", JSON.stringify(request.method))
+    console.log("request.query : ", JSON.stringify(request.query))
+    console.log("request.body : ", JSON.stringify(request.body))
+    response.set("content-type", "text/xml");
+    if (request.query.exiting !== "true" && request.query.sip_from_uri) {
+        /**
+         * Handle calls from SIP here.
+         *
+         * Just pass the call to the PSTN
+         */
 
-    var actionStream ={
-        action: "stream",
-        streamUrl: [
-            "https://nexmo-community.github.io/ncco-examples/assets/voice_api_audio_streaming.mp3"
-        ]
+        var from_number = request.query.destination_number.split('_')[0];
+        var destination_number = request.query.destination_number.split('_')[1];
+
+        let xml =
+            "<document>\n" +
+            " <work>\n" +
+            "   <dial caller-id-number='" + from_number + "'>\n" +
+            "     <number>" + destination_number + "</number>\n" +
+            "   </dial>\n" +
+            "   <hangup/>\n" +
+            " </work>\n" +
+            "</document>\n";
+
+        response.send(xml);
+        return;
     }
 
-    var actionConversation = {
-        action: "conversation",
-        name: "conversation-"+req.query.conference_id,
-        startOnEnter: "false",
-        musicOnHoldUrl: ["https://nexmo-community.github.io/ncco-examples/assets/phone-ringing.mp3"]
-    };
+    if (request.query.exiting !== "true" && !request.query.sip_from_uri) {
+        /**
+         * Handle calls from the PSTN here..
+         *
+         * Say hello then hangup
+         */
+        let xml =
+            "<document>\n" +
+            " <work>\n" +
+            "   <answer />\n" +
+            "   <wait>2</wait>\n" +
+            "   <speak>Hello there, we are trying to connect you</speak>\n" +
+            "   <dial>\n" +
+            "     <sipaccount>KLM-demo1</sipaccount>\n" +
+            "   </dial>\n" +
+            "   <hangup/>\n" +
+            " </work>\n" +
+            "</document>\n";
 
-    var ncco = [];
-    ncco.push(actionTalk);
-    //ncco.push(actionStream);
+        response.send(xml);
+        return;
+    }
 
-    res.json(ncco);
+    response.status(200);
+    response.send("");
 });
 
 
